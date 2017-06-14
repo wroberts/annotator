4ff968115e9496953e66c01a83c67697ed8c66c2
Will Roberts <will.roberts@rz.hu-berlin.de>
2017-06-14 13:20:05 +0200

    REST API: switch to marshmallow

--- annotator/annotations/views.py
+++ annotator/annotations/views.py
@@ -2,39 +2,60 @@
 """User views."""
 from flask import Blueprint, render_template
 from flask_login import login_required
-from flask_restful import Api, Resource, url_for, fields, marshal_with, reqparse
+from flask_restful import Api, Resource
+from marshmallow import Schema, fields
 from annotator.annotations.models import Annotation, Clause
 
 blueprint = Blueprint('annotation', __name__, url_prefix='/api', static_folder='../static')
 api = Api(blueprint)
 
-comp_fields = {
-    "type": fields.String(),
-    "begin": fields.Integer(),
-    "end": fields.Integer()
-    }
+class CompSchema(Schema):
+    """Marshmallow schema for SynArg and AspInd objects."""
 
-anno_fields = {
-    "invalid": fields.String(attribute=lambda annotation: annotation.invalid.name),
-    "stative": fields.String(attribute=lambda annotation: annotation.stative.name),
-    "bounded": fields.String(attribute=lambda annotation: annotation.bounded.name),
-    "change": fields.String(attribute=lambda annotation: annotation.change.name),
-}
+    type = fields.Str()
+    begin = fields.Int()
+    end = fields.Int()
 
-clause_fields = {
-    "id": fields.Integer(),
-    "sentence": fields.List(fields.String, attribute=lambda clause: clause.text.split()),
-    "verb-index": fields.Integer(attribute="verb_index"),
-    "verb-comps": fields.List(fields.Nested(comp_fields), attribute="synargs"),
-    "aspectual-indicators": fields.List(fields.Nested(comp_fields), attribute="synargs"),
-    "last-annotation-date": fields.DateTime(
-        dt_format='rfc822',
-        attribute=lambda clause: clause.annotation.created_at if clause.annotation else None),
-    "annotation": fields.Nested(anno_fields, allow_null=True),
-}
+
+class AnnoSchema(Schema):
+    """Marshmallow schema for Annotation objects."""
+
+    invalid = fields.Function(lambda annotation: annotation.invalid.name)
+    stative = fields.Function(lambda annotation: annotation.stative.name)
+    bounded = fields.Function(lambda annotation: annotation.bounded.name)
+    change = fields.Function(lambda annotation: annotation.change.name)
+
+
+class ClauseSchema(Schema):
+    """Marshmallow schema for Clause objects."""
+
+    id = fields.Integer()
+    sentence = fields.Function(lambda clause: clause.text.split())
+    verb_index = fields.Int(load_from='verb-index', dump_to='verb-index')
+    synargs = fields.List(fields.Nested(CompSchema),
+                          load_from='verb-comps', dump_to='verb-comps')
+    aspinds = fields.List(fields.Nested(CompSchema),
+                          load_from='aspectual-indicators', dump_to='aspectual-indicators')
+    last_annotation_date = fields.DateTime(format='iso', missing=None,
+                                           load_from='last-annotation-date',
+                                           dump_to='last-annotation-date')
+    annotation = fields.Nested(AnnoSchema, missing=None)
+
+
+def marshal(clause, annotation):
+    """
+    Use Marshmallow to generate a JSON representation of a
+    clause-annotation pair for a given user.
+    """
+    clause.annotation = annotation
+    clause.last_annotation_date = None
+    if annotation is not None:
+        clause.last_annotation_date = annotation.created_at
+    return ClauseSchema().dump(clause)
 
 class ClauseRsc(Resource):
-    @marshal_with(clause_fields)
+    """REST API for interacting with clauses and annotations."""
+
     def get(self, clause_id):
         """
         Returns the given Clause, with the most recent Annotation that
@@ -47,20 +68,23 @@ class ClauseRsc(Resource):
                       .filter(Annotation.user_id == 2)
                       .order_by(Annotation.created_at.desc())
                       .first())
-        clause.annotation = annotation
-        return clause
+        return marshal(clause, annotation)
 
-    @marshal_with(clause_fields)
     def put(self, clause_id):
+        """
+        Add a new annotation for the given clause, parsed out of the HTTP
+        PUT request object.
+        """
         # parse the PUT body
-        parser = reqparse.RequestParser()
-        parser.add_argument('invalid')
-        parser.add_argument('stative', type=int, help='Rate cannot be converted')
-        parser.add_argument('bounded')
-        parser.add_argument('change')
-        args = parser.parse_args()
-        todos[todo_id] = request.form['data']
-        return {todo_id: todos[todo_id]}
+        #parser = reqparse.RequestParser()
+        #parser.add_argument('invalid')
+        #parser.add_argument('stative', type=int, help='Rate cannot be converted')
+        #parser.add_argument('bounded')
+        #parser.add_argument('change')
+        #args = parser.parse_args()
+        #todos[todo_id] = request.form['data']
+        #return {todo_id: todos[todo_id]}
+        pass
 
 api.add_resource(ClauseRsc, '/clauses/<int:clause_id>')
 
