aa880b22bd2e77dc28420059b9a88abe24985cf8
Will Roberts <will.roberts@rz.hu-berlin.de>
2017-06-15 14:20:13 +0200

    js/index.js: fix

--- js/index.js
+++ js/index.js
@@ -1,18 +1,9 @@
 /* Main Javascript file for annotator */
 
-//import 'jquery/dist/jquery.min.js';
-//import 'bootstrap/dist/js/bootstrap.min.js';
-
-//import 'jquery';
-//import 'bootstrap';
-
-//var jQuery = require('./node_modules/jquery/dist/jquery.min.js');
-//var bootstrap = require('./node_modules/bootstrap/dist/js/bootstrap.min.js');
-
 // JS modules
 require("jquery");
 require("font-awesome-webpack");
 require("bootstrap");
 
-require('./js/plugins.js');
-require('./js/script.js');
+require('./plugins.js');
+require('./script.js');
