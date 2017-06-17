248622535c04321644fa8d79048026488f868df7
Will Roberts <will.roberts@rz.hu-berlin.de>
2017-06-17 15:56:21 +0200

    displaying sentence on page

--- js/app.js
+++ js/app.js
@@ -1,24 +1,20 @@
 import angular from 'angular';
+import 'angular-resource';
 import 'angular-route';
 
+import ClauseService from './service';
 import template from './interface.html';
 
-angular.module('Annotator', ['ngRoute'])
+const SPAN_TITLES = {
+  verb: 'Main verb of the clause',
+  subj: 'Subject',
+  obj: 'Direct object',
+  advp_for: 'Aspectual (for)',
+};
 
-  .directive('dashboard', () => {
-    return {
-      restrict: 'E',
-      transclude: true,
-      scope: {},
-      controller: ($location) => {
-        // todo
-        console.log('instantiating annotation interface');
-        console.log($location.path());
-      },
-      template: '<h3>Hello from Angular!</h3>',
-      replace: true,
-    };
-  })
+angular.module('Annotator', ['ngRoute', 'ngResource'])
+
+  .factory('Clauses', ClauseService)
 
   .config(($locationProvider, $routeProvider) => {
     $locationProvider
@@ -27,9 +23,57 @@ angular.module('Annotator', ['ngRoute'])
     $routeProvider
       .when('/:clauseId', {
         restrict: 'E',
-        controller: ($scope, $routeParams) => {
-          $scope.name = 'AnnotationController';
-          $scope.params = $routeParams;
+        controller: ($scope, $routeParams, Clauses) => {
+          // make the call to the REST API to fetch the clause object
+          $scope.spans = [];
+          Clauses.get({ id: $routeParams.clauseId },
+                      (clause) => {
+                        const spanMap = clause.sentence.map(() => undefined);
+                        for (const comp of clause['aspectual-indicators']) {
+                          for (let i = comp.begin; i < comp.end; i += 1)
+                            spanMap[i] = comp.type;
+                        }
+                        for (const comp of clause['verb-comps']) {
+                          for (let i = comp.begin; i < comp.end; i += 1)
+                            spanMap[i] = comp.type;
+                        }
+                        spanMap[clause['verb-index']] = 'verb';
+                        $scope.spanMap = spanMap;
+                        const spans = [];
+                        let lastClass;
+                        let currentSpan = { wclass: undefined, words: [] };
+                        for (let i = 0; i < clause.sentence.length - 1; i += 1)
+                        {
+                          const word = clause.sentence[i];
+                          const wclass = spanMap[i];
+                          if (wclass === lastClass) {
+                            currentSpan.words.push(word);
+                          } else {
+                            lastClass = wclass;
+                            if (currentSpan.words) {
+                              currentSpan.idx = spans.length;
+                              currentSpan.title = SPAN_TITLES[currentSpan.wclass];
+                              if (currentSpan.title) currentSpan.cclass = 'help';
+                              currentSpan.words = currentSpan.words.join(' ');
+                              spans.push(currentSpan);
+                            }
+                            currentSpan = { wclass, words: [word] };
+                          }
+                        }
+                        if (currentSpan.words) {
+                          currentSpan.idx = spans.length;
+                          currentSpan.title = SPAN_TITLES[currentSpan.wclass];
+                          if (currentSpan.title) currentSpan.cclass = 'help';
+                          currentSpan.words = currentSpan.words.join(' ');
+                          spans.push(currentSpan);
+                        }
+                        $scope.spans = spans;
+                      });
+          // we use the Clause service's cache to access the clause
+          // object in the page
+          $scope.cached = Clauses.cache;
+          $scope.isClean = Clauses.isClean;
+          $scope.save = Clauses.save;
         },
         template,
       });
---
+++ js/service.js
@@ -0,0 +1,47 @@
+export default ($resource) => {
+
+  const ClauseService = $resource(
+    '/api/clauses/:id',
+    {},
+    { update: { method: 'PUT' } });
+
+  ClauseService.cache = { clause: undefined, original: undefined };
+
+  const oldGet = ClauseService.get;
+  ClauseService.get = (params, cb) => {
+    ClauseService.cache.clause = undefined;
+    ClauseService.cache.original = undefined;
+    return oldGet(params, (clause) => {
+      ClauseService.cache.original = clause;
+      ClauseService.cache.clause = angular.copy(clause);
+      if (cb) {
+        cb(clause);
+      }
+    });
+  };
+
+  ClauseService.save = (cb) => {
+    if (ClauseService.cache.original) {
+      angular.extend(ClauseService.cache.original, ClauseService.cache.clause);
+      ClauseService.cache.original.$update((response) => {
+        ClauseService.cache.original = response;
+        ClauseService.cache.clause = angular.copy(response);
+        if (cb) {
+          cb(response);
+        }
+      });
+    }
+  };
+
+  ClauseService.isClean = () => {
+    return ClauseService.cache.original && angular.equals(ClauseService.cache.original,
+                                                          ClauseService.cache.clause);
+  };
+
+  ClauseService.prototype.$update = function (cb) {  // eslint-disable-line
+    return ClauseService.update({ id: this._id }, this, cb);  // eslint-disable-line
+  };
+
+  return ClauseService;
+
+}
