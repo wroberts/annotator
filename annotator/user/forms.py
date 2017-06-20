9ed761ce4702799ad0878ff9b6c336ec80ac014e
Will Roberts <will.roberts@rz.hu-berlin.de>
2017-06-20 13:45:09 +0200

    require password confirmation on /register

--- annotator/user/forms.py
+++ annotator/user/forms.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 """User forms."""
-from flask_security.forms import ConfirmRegisterForm, RegisterForm
+from flask_security.forms import ConfirmRegisterForm, PasswordConfirmFormMixin, RegisterForm
 from wtforms import StringField
 from wtforms.validators import DataRequired
 
 
-class ExtendedConfirmRegisterForm(ConfirmRegisterForm):
+class ExtendedConfirmRegisterForm(ConfirmRegisterForm, PasswordConfirmFormMixin):
     first_name = StringField('First Name', [DataRequired()])
     last_name = StringField('Last Name', [DataRequired()])
 
