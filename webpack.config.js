14a0d013abd1e3738a46b284525e46d44db36559
Will Roberts <will.roberts@rz.hu-berlin.de>
2017-10-14 12:49:29 +0200

    package.json: update dependencies for greenkeeper

--- webpack.config.js
+++ webpack.config.js
@@ -23,21 +23,21 @@ module.exports = {
     app_css: [
       './node_modules/font-awesome/css/font-awesome.css',
       './node_modules/bootstrap/dist/css/bootstrap.css',
-      './assets/css/style.css'
-    ]
+      './assets/css/style.css',
+    ],
   },
   output: {
-    path: __dirname + '/annotator/static',
-    publicPath: publicHost + '/static/',
+    path: `${__dirname}/annotator/static`,
+    publicPath: `${publicHost}/static/`,
     filename: '[name].[hash].js',
-    chunkFilename: '[id].[hash].js'
+    chunkFilename: '[id].[hash].js',
   },
   resolve: {
-    extensions: ['.js', '.jsx', '.css']
+    extensions: ['.js', '.jsx', '.css'],
   },
   devtool: debug ? 'inline-sourcemap' : 'source-map',
   devServer: {
-    headers: { 'Access-Control-Allow-Origin': '*' }
+    headers: { 'Access-Control-Allow-Origin': '*' },
   },
   module: {
     loaders: [
@@ -46,24 +46,24 @@ module.exports = {
       { test: /\.css$/, loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' }) },
       { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
       { test: /\.(ttf|eot|svg|png|jpe?g|gif|ico)(\?.*)?$/i,
-        loader: 'file-loader?context=' + rootAssetPath + '&name=[path][name].[hash].[ext]' },
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader', query: { presets: ['es2015'], cacheDirectory: true } },
+        loader: `file-loader?context=${rootAssetPath}&name=[path][name].[hash].[ext]` },
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader', query: { presets: ['env'], cacheDirectory: true } },
     ]
   },
   plugins: [
     new ExtractTextPlugin('[name].[hash].css'),
     new webpack.ProvidePlugin({ $: 'jquery',
                                 jQuery: 'jquery' }),
-    new ManifestRevisionPlugin(__dirname + '/annotator/webpack/manifest.json', {
+    new ManifestRevisionPlugin(`${__dirname}/annotator/webpack/manifest.json`, {
       rootAssetPath,
-      ignorePaths: ['/js', '/css']
+      ignorePaths: ['/js', '/css'],
     }),
   ].concat(debug ? [] : [
     // production webpack plugins go here
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
+        'NODE_ENV': JSON.stringify('production'),
+      },
     }),
-  ])
+  ]),
 };
