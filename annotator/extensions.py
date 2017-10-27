61870683239f1bb1a877f890afc3984b05b0fed4
Will Roberts <will.roberts@rz.hu-berlin.de>
2017-10-27 16:22:08 +0200

    flask_gitversion

--- annotator/extensions.py
+++ annotator/extensions.py
@@ -11,10 +11,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_webpack import Webpack
 from flask_wtf.csrf import CSRFProtect
 
+from .flask_gitversion import GitVersion
+
 babel = Babel()
 compress = Compress()
 csrf_protect = CSRFProtect()
 db = SQLAlchemy()
+gitversion = GitVersion()
 security = Security()
 migrate = Migrate()
 mail = Mail()
---
+++ annotator/flask_gitversion/__init__.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Make the git hash available in a Flask application.
+
+flask_gitversion/__init__.py
+"""
+
+import os
+import subprocess
+
+
+# https://stackoverflow.com/a/40170206/1062499
+# Return the git revision as a string
+def git_version():
+    """Get the git hash."""
+    def _minimal_ext_cmd(cmd):
+        # construct minimal environment
+        env = {}
+        for k in ['SYSTEMROOT', 'PATH']:
+            v = os.environ.get(k)
+            if v is not None:
+                env[k] = v
+        # LANGUAGE is used on win32
+        env['LANGUAGE'] = 'C'
+        env['LANG'] = 'C'
+        env['LC_ALL'] = 'C'
+        out = subprocess.Popen(cmd, stdout=subprocess.PIPE, env=env).communicate()[0]
+        return out
+
+    try:
+        out = _minimal_ext_cmd(['git', 'rev-parse', '--short', 'HEAD'])
+        git_revision = out.strip().decode('ascii')
+    except OSError:
+        git_revision = 'Unknown'
+
+    return git_revision
+
+
+GIT_VERSION = git_version()
+
+
+class GitVersion(object):
+    """Insert the git hash into the global context."""
+
+    def __init__(self, app=None):
+        """Constructor."""
+        self.app = app
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        """Initialse extension on the given app."""
+        @app.context_processor
+        def gitversion_processor():
+            return dict(git_version=GIT_VERSION)
